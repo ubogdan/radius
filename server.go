d052c0b2bfc7d831f0e1b405c34ef8b7aa3240e3
Bogdan Ungureanu <ubogdan@gmail.com>
2020-10-23 23:03:41 +0300

    add HandlerFunc

--- server.go
+++ server.go
@@ -45,6 +45,17 @@ type Handler interface {
 	ServeRADIUS(ResponseWriter, *Request)
 }
 
+// The HandlerFunc type is an adapter to allow the use of
+// ordinary functions as RAUDIUS handlers. If f is a function
+// with the appropriate signature, HandlerFunc(f) is a
+// Handler that calls f.
+type HandlerFunc func(ResponseWriter, *Request)
+
+// ServeHTTP calls f(w, r).
+func (f HandlerFunc) ServeRADIUS(w ResponseWriter, r *Request) {
+	f(w, r)
+}
+
 type responseWriter struct {
 	// listener that received the packet
 	conn net.PacketConn
